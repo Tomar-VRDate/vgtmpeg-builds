65de354754c6b09529f187f6d81617631b3ae2aa
Alberto <alberto@ubuntito.(none)>
2010-03-27 15:49:29 -0700

    added function

--- build_all.sh
+++ build_all.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-rebuild=no 
+rebuild=no
 
 gst_version=0.10.25
 gstpb_version=0.10.25
@@ -11,8 +11,8 @@ gstpu_version=0.10.13
 gstpbad_version=0.10.17
 gnonlin_version=0.10.9
 gstpy_version=0.10.12
-glib_version=2.16.3
-libxml2_version=2.6.32
+glib_version=2.22.5
+libxml2_version=2.7.7
 libiconv_version=1.13.1
 gettext_version=0.17
 
@@ -43,43 +43,35 @@ export LDFLAGS="-L${prefix}/lib"
 test -d $srcdir || mkdir $srcdir
 cd $srcdir
 
-#LIBICONV
-libiconv_base=libiconv-$libiconv_version
-libiconv_tar=$libiconv_base.tar.gz
-libiconv_src=$RESOURCE_URL_BASE/$libiconv_tar
-if [ ! -f $libiconv_tar ]
-then
-    wget $libiconv_src
-    rm -rf $libiconv_base
-    tar -xzvf $libiconv_tar
-    cd $libiconv_base
-    ./configure \
-    --host=i586-mingw32msvc \
-    --build=i486-linux-gnu \
-    --prefix=$prefix
-    make
-    make install
-    cd ..
-fi
+#get_and_build
+# $1 library_name
+# $2 library_version
+# $3 configure extra options
+function get_and_build {
+    base=$1-$2
+    tar=$base.tar.gz
+    src=$RESOURCE_URL_BASE/$tar
+    copt="--host=i586-mingw32msvc \
+        --build=i486-linux-gnu \
+        --prefix=$prefix \
+        $3"
+    if [ ! -f $tar ]
+    then
+        wget $src
+        rm -rf $base
+        tar -xzvf $tar
+        cd $base
+        ./configure $copt 
+        make
+        make install
+        cd ..
+    fi
+}
 
-#GETTEXT
-gettext_base=gettext-$gettext_version
-gettext_tar=$gettext_base.mingw32patched.tar.gz
-gettext_src=$RESOURCE_URL_BASE/$gettext_tar
-if [ ! -f $gettext_tar ]
-then
-    wget $gettext_src
-    rm -rf $gettext_base
-    tar -xzvf $gettext_tar
-    cd $gettext_base
-    ./configure \
-    --host=i586-mingw32msvc \
-    --build=i486-linux-gnu \
-    --prefix=$prefix
-    make
-    make install
-    cd ..
-fi
+get_and_build libiconv $libiconv_version
+get_and_build gettext  $gettext_version 
+get_and_build glib     $glib_version 
+get_and_build libxml2  $libxml2_version "--without-python" 
 
 
 #return to origin
