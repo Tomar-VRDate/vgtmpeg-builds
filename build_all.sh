34085fa63653a4622bed7f01f2db2eb9af50369c
Alberto <alberto@ubuntito.(none)>
2010-03-28 02:01:56 -0700

    added support for gstreamr win32 build

--- build_all.sh
+++ build_all.sh
@@ -15,12 +15,17 @@ glib_version=2.22.5
 libxml2_version=2.7.7
 libiconv_version=1.13.1
 gettext_version=0.17
+#gstreamer_git_tagname=NELARELEASE-0.0.1
+gstreamer_git_tagname=RELEASE-0.10.28
 
 prefix=$(pwd)/build
 srcdir=$(pwd)/src
 
 RESOURCE_URL_BASE=http://nelalabs.com/res
-
+GITREPO_BASE=git@nelalabs.unfuddle.com:nelalabs
+CONFIGURE_BASE_OPT="--host=i586-mingw32msvc \
+        --prefix=$prefix "
+#        --build=i486-linux-gnu \
 
 export CC=i586-mingw32msvc-gcc
 export LD=i586-mingw32msvc-ld
@@ -51,10 +56,8 @@ function get_and_build {
     base=$1-$2
     tar=$base.tar.gz
     src=$RESOURCE_URL_BASE/$tar
-    copt="--host=i586-mingw32msvc \
-        --build=i486-linux-gnu \
-        --prefix=$prefix \
-        $3"
+    copt=$3
+
     if [ ! -f $tar ]
     then
         wget $src
@@ -68,10 +71,32 @@ function get_and_build {
     fi
 }
 
-get_and_build libiconv $libiconv_version
-get_and_build gettext  $gettext_version 
-get_and_build glib     $glib_version 
-get_and_build libxml2  $libxml2_version "--without-python" 
+#gst_get_and_build
+# $1 library_name
+# $2 library_tag_name
+# $3 configure options
+function gst_get_and_build {
+    name=$1
+    tagname=$2
+    copt=$3
+    if [ ! -d $name ]
+    then
+        rm -rf $name
+        git clone $GITREPO_BASE/$name
+        cd $name
+        git checkout $tagname
+        ./autogen.sh -- $copt 
+        make
+        make install
+        cd ..
+    fi
+}
+
+get_and_build     libiconv   $libiconv_version      "$CONFIGURE_BASE_OPT"
+get_and_build     gettext    $gettext_version       "$CONFIGURE_BASE_OPT"
+get_and_build     glib       $glib_version          "$CONFIGURE_BASE_OPT"
+get_and_build     libxml2    $libxml2_version       "$CONFIGURE_BASE_OPT --without-python" 
+gst_get_and_build gstreamer  $gstreamer_git_tagname "$CONFIGURE_BASE_OPT --enable-gtk-doc=no"
 
 
 #return to origin
